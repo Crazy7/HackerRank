d0145dbc6421ecd0427628ea52325f6cf03c934c
Crazy7 <fangshanzhang@live.com>
2016-08-05 18:01:27 +0800

    add LeftRotation

---
+++ LeftRotation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+
+        var NAndD = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        var array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        LeftRotate(array, NAndD[1]);
+    }
+
+    private static void LeftRotate(int[] a, int d)
+    {
+        var n = a.Length;
+        if (d < n)
+        {
+            for (int i = d; i < n; i++) Console.Write(a[i] + " ");
+            for (int i = 0; i < d; i++) Console.Write(a[i] + " ");
+        }
+        else
+        {
+            for (int i = n - 1; i >= 0; i--) Console.Write(a[i] + " ");
+        }
+
+    }
+}
