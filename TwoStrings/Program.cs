48674985c24df9b57f1e0b448d1abcb2445b2ca7
Gavin Fang <fangshanzhang@live.com>
2016-08-07 10:23:30 +0800

    TwoStrings

---
+++ TwoStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        var caseCount= int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < caseCount; i++)
+        {
+            var a = Console.ReadLine();
+            var b = Console.ReadLine();
+
+            var hasSameSubString = HasSameSubString(a, b);
+            var msg = hasSameSubString ? "YES" : "NO";
+
+            Console.WriteLine(msg);
+        }
+    }
+
+    private static bool HasSameSubString(string a, string b)
+    {
+        return a.Intersect(b).Count() > 0;
+    }
+}
