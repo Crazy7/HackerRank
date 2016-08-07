07329394659a34dab72203e46942be3788f45fe9
Gavin Fang <fangshanzhang@live.com>
2016-08-07 11:55:51 +0800

    Game of Thrones I

---
+++ GameOfThrones_I/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    public class Solution
+    {
+        public static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+
+            var ret = IsPalindrome(str);
+            var msg = ret ? "YES" : "NO";
+            Console.WriteLine(msg);
+        }
+
+        private static bool IsPalindrome(string str)
+        {
+            return str.GroupBy(_ => _).Where(_ => _.Count() % 2 == 1).Count() <= 1;
+        }
+    }
+}
