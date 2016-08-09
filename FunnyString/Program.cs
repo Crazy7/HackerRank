3723dc758e7e23457ef17a7f088fa0c3e6f9ac2b
Gavin Fang <fangshanzhang@live.com>
2016-08-09 21:34:33 +0800

    Funny String

---
+++ FunnyString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        var caseCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < caseCount; i++)
+        {
+            var cur = Console.ReadLine();
+            var ret = IsFunny(cur);
+
+            var msg = ret ? "Funny" : "Not Funny";
+            Console.WriteLine(msg);
+        }
+    }
+
+    private static bool IsFunny(string str)
+    {
+        var len = str.Length;
+
+        for (int i = 1; i < len; i++)
+        {
+            var a = str[i] - str[i - 1];
+            var b = str[len - 1 - i] - str[len - i];
+
+            if (Math.Abs(a) != Math.Abs(b)) return false;
+        }
+
+        return true;
+    }
+}
