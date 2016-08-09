d9c81f22fa062e5b5cf9e6ce1672296a0145aa26
Gavin Fang <fangshanzhang@live.com>
2016-08-09 21:10:02 +0800

    day6 - Let's Review

---
+++ LetsReview/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        var caseCount = int.Parse(Console.ReadLine());
+        for (var i = 0; i < caseCount; i++)
+        {
+            var cur = Console.ReadLine();
+
+            var evenSub = cur.Where((_v, _i) => _i % 2 == 0);
+            var oddSub = cur.Where((_v, _i) => _i % 2 == 1);
+
+            var ret = $"{string.Concat(evenSub)}  {string.Concat(oddSub)}";
+
+            Console.WriteLine(ret);
+        }
+    }
+}
