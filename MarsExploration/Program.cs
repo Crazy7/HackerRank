92501ad324992ab6db5b24452dda03e0a9a9a515
Gavin Fang <fangshanzhang@live.com>
2016-08-09 21:43:45 +0800

    Mars Exploration

---
+++ MarsExploration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        string S = Console.ReadLine();
+
+        var changedCount = 0;
+        for (var i = 0; i < S.Length; i += 3)
+        {
+            if (S[i] != 'S') changedCount++;
+            if (S[i + 1] != 'O') changedCount++;
+            if (S[i + 2] != 'S') changedCount++;
+        }
+
+        Console.WriteLine(changedCount);
+    }
+}
