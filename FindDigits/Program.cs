dfe490512be6039f0d53256efbd745ef272b22f8
Crazy7 <fangshanzhang@live.com>
2016-08-11 22:58:50 +0800

    FindDigits

---
+++ FindDigits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            var ret = FindDigits(n);
+            Console.WriteLine(ret);
+        }
+    }
+
+    private static int FindDigits(int n)
+    {
+        return n.ToString().ToCharArray()
+                .Where(_ => _ != '0')
+                .Select(_ => _.ToString())
+                .Select(int.Parse)
+                .Where(_ => n % _ == 0).Count();
+    }
+}
