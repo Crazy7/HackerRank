585eb1c9ce0861d7b0979309aadc8155c3f4bf25
Crazy7 <fangshanzhang@live.com>
2016-08-13 09:25:56 +0800

    Day10 - Binary Numbers

---
+++ BinaryNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        if (n == 0)
+        {
+            Console.WriteLine(0);
+            return;
+        }
+
+        var binary = Convert.ToString(n, 2);
+
+        var ret = 0;
+        var i = 0;
+        while (i < binary.Length)
+        {
+            var cur = binary[i];
+
+            if (cur != '1')
+            {
+                i++;
+                continue;
+            }
+
+            var j = i;
+            var counter = 0;
+
+            while (j < binary.Length)
+            {
+                if (binary[j] != '1') break;
+                else counter++;
+
+                j++;
+            }
+
+            ret = Math.Max(ret, counter);
+
+            if (j > i) i = j;
+            else i++;
+        }
+
+        Console.WriteLine(ret);
+    }
+}
