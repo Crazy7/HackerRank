4d21dafb066db49542d45c6af65acc4b25a83cf0
Gavin Fang <fangshanzhang@live.com>
2016-08-21 19:07:09 +0800

    Cut the sticks

---
+++ CutTheSticks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = arr_temp.Select(int.Parse).ToArray();
+
+        const int max = 1000;
+        var map = new int[max];
+        for (var i = 0; i < max; i++) map[i] = 0;
+        for (var i = 0; i < n; i++)
+        {
+            var x = i;
+            var v = arr[x];
+            map[v]++;
+        }
+        Console.WriteLine(n);
+        for (var i = 0; i < max; i++)
+        {
+            var cur = map[i];
+            if (cur == 0) continue;
+
+            n -= cur;
+
+            if (n > 0) Console.WriteLine(n);
+
+        }
+    }
+}
