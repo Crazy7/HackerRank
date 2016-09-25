4bb6fb83cbcbb4b28a82814bd6d1e17a5a247a55
Crazy7 <fangshanzhang@live.com>
2016-09-25 10:37:57 +0800

    Inserting a Node Into a Sorted Doubly Linked List

---
+++ LinkedList/InsertNodeToSortedDoublyList/Solution.cpp
@@ -0,0 +1,45 @@
+URL : https://www.hackerrank.com/challenges/insert-a-node-into-a-sorted-doubly-linked-list?h_r=next-challenge&h_v=zen
+
+/*
+    Insert Node in a doubly sorted linked list 
+    After each insertion, the list should be sorted
+   Node is defined as
+   struct Node
+   {
+     int data;
+     Node *next;
+     Node *prev;
+   }
+*/
+Node* SortedInsert(Node *head,int data)
+{  
+    Node *cur = new Node();  
+    cur->data = data;  
+    cur->next = cur->prev = NULL;  
+    if(head == NULL) 
+        return cur;  
+    Node *pos = head;  
+    while(pos != NULL && pos->data <= data)  
+        pos = pos->next;  
+    if(pos == NULL)  
+    {  
+        Node *tail = head;  
+        while(tail->next != NULL)  
+            tail = tail->next;  
+        tail->next = cur;  
+        cur->prev = tail;  
+        return head;  
+    }  
+    Node *pre = pos->prev;  
+    if(pre == NULL)  
+    {  
+        cur->next = head;  
+        head->prev = cur;  
+        return cur;  
+    }  
+    pre->next = cur;  
+    cur->prev = pre;  
+    cur->next = pos;  
+    pos->prev = cur;  
+    return head;  
+}
