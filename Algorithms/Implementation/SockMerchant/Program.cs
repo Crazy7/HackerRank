bdc13e7b25854ade326a700488626465f0f168d7
Crazy7 <fangshanzhang@live.com>
2016-10-07 22:43:40 +0800

    Sock Merchant

---
+++ Algorithms/Implementation/SockMerchant/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        var n = Convert.ToInt32(Console.ReadLine());
+        var c = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        var ret = c.GroupBy(_ => _).Select(_ => _.Count()).Where(_ => _ > 1).Select(_ => _ / 2).Sum();
+
+        Console.WriteLine(ret);
+    }
+}
